59f2d3949cb2f17c81453a605cf6e38b15cf5b67
Doc Norton <michael@docondev.com>
2016-03-30 14:12:02 -0500

    base entry by day on date user provided

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,7 @@ class Entry < ActiveRecord::Base
   belongs_to :user
 
   def self.by_day(date)
-    where(:created_at => (date.beginning_of_day..date.end_of_day))
+    where(:date => (date.beginning_of_day..date.end_of_day))
   end
 
   def self.today
