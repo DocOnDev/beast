b02ee6901d7ebaf97d906eb87bdc829d61305efd
Doc Norton <michael@docondev.com>
2016-04-06 09:23:08 -0400

    Closes #17; Redirect recipe add/edit back to recipe list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -77,7 +77,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
+        format.html { redirect_to recipes_url, notice: "#{@recipe.name} recipe was successfully created." }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new }
@@ -91,7 +91,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
+        format.html { redirect_to recipes_url, notice: "#{@recipe.name} recipe was successfully updated." }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit }
