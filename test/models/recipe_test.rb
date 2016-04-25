83d76c5ec88ca24d721718b14c21ded0443d79c0
Doc Norton <michael@docondev.com>
2016-04-25 17:13:39 -0500

    Tests recipe model

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "search" do
+    found_items = Recipe.search("beef")
+    assert found_items[0].name == "Soup"
+
+    found_items = Recipe.search("Meatloaf")
+    assert found_items[0].name == "Meatloaf"
+  end
 end
