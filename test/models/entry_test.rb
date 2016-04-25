4080ee66d9202f3caa88e4bee1810aebe9decbb0
Doc Norton <michael@docondev.com>
2016-04-25 17:07:36 -0500

    Tests entry model

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "today" do
+    entries = Entry.today
+    assert entries[0].portion == 9.99
+  end
+
+  test "by_day" do
+    target_date = Date.new(2016,03,27)
+    entries = Entry.by_day(target_date)
+    assert entries[0].portion == 29.99
+  end
 end
