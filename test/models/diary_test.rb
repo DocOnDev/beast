9dce52105e1518cc54cfca93779db034dcd1eb66
Doc Norton <michael@docondev.com>
2016-04-25 16:58:55 -0500

    Tests diary model

--- test/models/diary_test.rb
+++ test/models/diary_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DiaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "today" do
+    diaries = Diary.today
+    assert diaries[0].sleep_score == 86
+  end
+
+  test "by_day" do
+    target_date = Date.new(2016,03,21)
+    diaries = Diary.by_day(target_date)
+    assert diaries[0].sleep_score == 91
+  end
 end
