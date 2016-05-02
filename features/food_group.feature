7321ecad4525087470f6b49358dac259f962ca99
Doc Norton <michael@docondev.com>
2016-05-02 11:01:38 -0500

    DRY food group update

--- features/food_group.feature
+++ features/food_group.feature
@@ -7,6 +7,11 @@ Feature: Food Group
 
   Scenario: Create a food group
     Given there are 10 food groups
-    When I create a food group with the name "Feature Food Group"
+    When I create a food group with the name "Create Scenario Food Group"
     And I visit the food groups page
-    Then I should see the "Feature Food Group" food group
+    Then I should see the "Create Scenario Food Group" food group
+
+  Scenario: Update a food group
+    Given a food group with name "Standard Food Group"
+    When I update the name to "Scenario Update Food Group"
+    Then I should see the "Scenario Update Food Group" food group
--- features/step_definitions/food_group_steps.rb
+++ features/step_definitions/food_group_steps.rb
@@ -11,7 +11,7 @@ When(/^I visit the food groups page$/) do
 end
 
 Then(/^I should see a list of (\d+) food groups$/) do |group_count|
-  # Hack - currently have 3 rows from residue in database due to other tests not cleaning up
+  #TODO: Clean this up - currently have 3 rows from residue in database due to other tests not cleaning up
   page.should have_css("table tbody tr", :count=>(group_count.to_i + 3))
 end
 
@@ -21,4 +21,14 @@ end
 
 Then(/^I should see the "([^"]*)" food group$/) do |group_name|
   page.should have_content(group_name)
+end
+
+Given(/^a food group with name "([^"]*)"$/) do |group_name|
+  @food_group = FactoryGirl.create(:food_group, name: group_name)
+end
+
+When(/^I update the name to "([^"]*)"$/) do |name|
+  visit edit_food_group_path(@food_group)
+  fill_in "food_group_name", :with => name
+  click_button "Update Food group"
 end
